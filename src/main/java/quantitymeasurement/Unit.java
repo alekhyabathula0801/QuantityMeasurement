d9dc735af36d8a2194fdbbe5963cedbe9fc4ae98
arun kumar <alekhya0801@gmail.com>
2020-06-04 23:29:14 +0530

    Added feature to add length quantities

--- src/main/java/quantitymeasurement/Unit.java
+++ src/main/java/quantitymeasurement/Unit.java
@@ -3,7 +3,7 @@ package quantitymeasurement;
 public enum Unit {
 
     FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETRE(1.0/2.54);
-    private Double inchConversionValue;
+    private final Double inchConversionValue;
 
     Unit(Double inchConversionValue) {
         this.inchConversionValue = inchConversionValue;
---
+++ src/main/java/quantitymeasurement/UnitAddition.java
@@ -0,0 +1,9 @@
+package quantitymeasurement;
+
+public class UnitAddition {
+
+    public double add(QuantityMeasurement quantityMeasurement1, QuantityMeasurement quantityMeasurement2) {
+        return quantityMeasurement1.value + quantityMeasurement2.value;
+    }
+
+}
