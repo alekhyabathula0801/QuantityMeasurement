73c2aff9236c6337401976150d7e2efb4a34257e
arun kumar <alekhya0801@gmail.com>
2020-06-05 13:58:22 +0530

    Added feature to add quantities of volume measurement

--- src/main/java/quantitymeasurement/UnitAddition.java
+++
@@ -1,9 +0,0 @@
-package quantitymeasurement;
-
-public class UnitAddition {
-
-    public double add(QuantityMeasurement quantityMeasurement1, QuantityMeasurement quantityMeasurement2) {
-        return quantityMeasurement1.value + quantityMeasurement2.value;
-    }
-
-}
