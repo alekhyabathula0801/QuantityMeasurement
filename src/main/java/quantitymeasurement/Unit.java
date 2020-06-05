797dcc6fd381eca911cb8f8342075490b78767ac
arun kumar <alekhya0801@gmail.com>
2020-06-05 18:13:42 +0530

    Refactored code using varags to initialize conversion values

--- src/main/java/quantitymeasurement/Unit.java
+++ src/main/java/quantitymeasurement/Unit.java
@@ -1,41 +1,32 @@
 package quantitymeasurement;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Unit {
 
-    FEET(304.8,"LENGTH"), INCH(25.4,"LENGTH"), YARD(914.4,"LENGTH"),CENTIMETRE(10.0,"LENGTH"), METRE(1000.0,"LENGTH"),
-    KILOMETRE(1000000.0,"LENGTH"), MILLIMETRE(1.0,"LENGTH"), LITRE(1000.0,"VOLUME"), GALLON(3785.41,"VOLUME"),
-    MILLILITRE(1.0,"VOLUME"), GRAM(1.0,"MASS"), KILOGRAM(1000.0,"MASS"),
-    TONNE(1000000.0,"MASS"), KELVIN(1.0,"TEMPERATURE",0.0,0.0),
-    CELCIUS(1.0,"TEMPERATURE",273.15,0.0), FAHRENHEIT(5.0/9.0,"TEMPERATURE",-32.0,273.15);
+    FEET("LENGTH",304.8), INCH("LENGTH",25.4), YARD("LENGTH",914.4),CENTIMETRE("LENGTH",10.0), METRE("LENGTH",1000.0),
+    KILOMETRE("LENGTH",1000000.0), MILLIMETRE("LENGTH",1.0), LITRE("VOLUME",1000.0), GALLON("VOLUME",3785.41),
+    MILLILITRE("VOLUME",1.0), GRAM("MASS",1.0), KILOGRAM("MASS",1000.0),
+    TONNE("MASS",1000000.0), KELVIN("TEMPERATURE",1.0,0.0,0.0),
+    CELCIUS("TEMPERATURE",1.0,273.15,0.0), FAHRENHEIT("TEMPERATURE",5.0/9.0,-32.0,273.15);
 
-    private final Double conversionValue;
     final String measurementType;
+    private List<Double> conversionValue;
 
-    Unit(Double conversionValue,String measurementType) {
-        this.conversionValue = conversionValue;
+    Unit(String measurementType,Double... conversionValue) {
         this.measurementType = measurementType;
-        temperatureConversion1 = null;
-        temperatureConversion2 = null;
-    }
-
-    private final Double temperatureConversion1;
-    private final Double temperatureConversion2;
-
-    Unit(Double conversionValue, String measurementType, Double temperatureConversion1, Double temperatureConversion2) {
-        this.conversionValue = conversionValue;
-        this.measurementType = measurementType;
-        this.temperatureConversion1 = temperatureConversion1;
-        this.temperatureConversion2 = temperatureConversion2;
+        this.conversionValue = Arrays.asList(conversionValue);
     }
 
     public double getConvertedValue(double value,Unit unit) {
         if(unit.measurementType != "TEMPERATURE")
-            return conversionValue*value;
-        return conversionValue*(value+ temperatureConversion1) + temperatureConversion2;
+            return conversionValue.get(0)*value;
+        return conversionValue.get(0)*(value+ conversionValue.get(1)) + conversionValue.get(2);
     }
 
     public Double getConversionValue() {
-        return conversionValue;
+        return conversionValue.get(0);
     }
 
 }
